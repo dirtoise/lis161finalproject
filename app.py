3b00c114667c8732c40aadb1743f4825c0dd9d82
dirtoise <82037590+dirtoise@users.noreply.github.com>
2021-06-21 06:12:02 +0800

    added a simple login function

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from data import *
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['name']
+        session['user'] = user
+        return redirect(url_for('user',))
+    else:
+        if 'user' in session:
+            return redirect(url_for('user',))
+        return render_template('login.html')
+
+@app.route('/user')
+def user():
+    if 'user' in session:
+        user = session['user']
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 @app.route('/menu')
 def basemenu():
